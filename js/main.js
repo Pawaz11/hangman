08eeb7b81b37a6472a6cd6c736c6adbdb2681338
TECH-A50 <tech-a50@TECH-A50s-MacBook-Air.local>
2018-06-06 11:00:49 +0100

    Game functional, TODO: Add modal for instructions and when game ends

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 //create an array to store the words
-var arrayOfWords = ["SCHOOL","LONDON","JAVASCRIPT","MACBOOK"];
+var arrayOfWords = ["JAVA","CSHARP","JAVASCRIPT","PYTHON","PHP","HTML","CSS"];
 var rand = Math.floor(Math.random() * arrayOfWords.length);
-var word = arrayOfWords[rand];
-var remainingCharacters = word.length;
+var word;
+var remainingCharacters;
 var correctChars = 0;
-var lives = 10;
+var lives = 0;
 var parts = 0;
 
 function hanging(){
@@ -15,11 +15,26 @@ function hanging(){
     }
     x++;
   });
-
-
 }
-function unhang(){
+
+//Create a reset function that will return the game back to its original state.
+function reset(){
   $('.hangman-items').css({"display":"none"});
+  $(".win").css({"opacity":"0"});
+  $(".lost").css({"opacity":"0"});
+  lives = 10;
+  $("h3").html("You have: " + lives + " lives left");
+  remainingCharacters = 0;
+  correctChars = 0;
+  parts = 0;
+  rand = Math.floor(Math.random() * arrayOfWords.length);
+  word= arrayOfWords[rand];
+  remainingCharacters = word.length;
+  $(".word").html('');
+  for (var i = 0; i <=remainingCharacters; i++) {//update the box with a space and underline for every remaining character
+      var content = $(".word").html();
+      $(".word").html(content+' _ ');
+    }
 }
 
 function updateDisplay() {
@@ -30,7 +45,7 @@ function updateDisplay() {
   }
 
   $(".word").html(updateContent);//set box to show the character player got correct
-  console.log('im updating this amount of chars '+remainingCharacters);
+  console.log('Updating this amount of chars '+remainingCharacters);
   for (var i = 0; i <remainingCharacters; i++) {//update the box with a space and underline for every remaining character
       var content = $(".word").html();
       $(".word").html(content+' _ ');
@@ -48,81 +63,32 @@ function matchWordCharsToClick(){
       updateDisplay();
       if (remainingCharacters==0) {
             console.log("You win!");
-            $("h2").css({"display": "inline-block"});
+            $(".win").fadeTo('slow',1,function(){});
 
         }
-    }else if (clickedCharacter != currentCharacter) {
-        lives-- ;
-        parts++;
-        hanging();
-        $("h3").html("You have: " + lives + " lives left");
+          //If the guess is wrong, a body part is added and score is decreased by 1.
+          }else if (clickedCharacter != currentCharacter) {
+              lives--;
+              parts++;
+              hanging();
+              $("h3").html("You have: " + lives + " lives left");
 
-        if (lives == 0){
-          $("h2").html("You lost!");
-          $("h2").css({"display":"inline-block"});
-          $("h2").css({"color": "red"});
+              if (lives == 0){
+                // $("h2").html("You lost!");
+                // $(".lost").css({"display":"block"});
+                // $("h2").css({"color": "red"});
+                $(".lost").fadeTo('slow',1,function(){});
         }
       }
-
-
-      for (var i = lives; i <= correctChars; i++) {
-        $( "div:nth-type-of()" ).css({"display": "block"});
-
-        // $(".scaffold-top").css({"display": "block"});
-        $(".scaffold-left").css({"display": "block"});
-        // $(".rope").css({"display": "block"});
-        // $(".head").css({"display": "block"});
-        // $(".leftarm").css({"display": "block"});
-        // $(".rightarm").css({"display": "block"});
-        // $(".torso").css({"display": "block"});
-        // $(".leftleg").css({"display": "block"});
-        // $(".rightleg").css({"display": "block"});
-        // $(".pelvis").css({"display": "block"});
-      }
-
-      // if (correctChars <= lives) {
-      // } else  if (true) {
-      //
-      // }else if (true) {
-      //
-      // }{
-      //
-      // }
   });
 }
 
 
 $(document).ready(function(){
   console.log("Dom loaded");
-  for (var i = 0; i <=remainingCharacters; i++) {//update the box with a space and underline for every remaining character
-      var content = $(".word").html();
-      $(".word").html(content+' _ ');
-    }
+  reset();
   matchWordCharsToClick();
-  unhang();
-// function javascript(){
-//   //gets the word school from array
-//   var word = arrayOfWords[2];
-//   addDivs(word);
-//   //event click for letters
-//   $(".letters").click(function(){
-//   //if the letter is correct, then hide the letter
-//   var alpha = $(this).html();
-//
-//   // for (var i = 0; i < word.length; i++) {
-//   //     var content = $(".word").html();
-//   //     $(".word").html(content+' _');
-//   // }
-// });
-// }
 
-// function alphabetClick(){
-//   $(".letters").click(function(){
-//    var alpha = $(".letters").innerText;
-//    if (alpha == "A") {
-//      $(".letters").hide();
-//    }
-//   })
 
   //Select a random word from the array and display underlines for the length of the word
     //Select word from the array
@@ -130,17 +96,5 @@ $(document).ready(function(){
     //Display word to screen
 
     //Underline characters for the length of the word
-
-
-  //create a function that will match the clicked letter to the letters in the word.
-
-
-  //If the letters match then hide the guessed letters.
-
-
-  //If the guess is wrong, a body part is added and score is decreased by 1.
-
-
-  //Create a reset function that will return the game back to its original state.
 
 });
